4c4c7bb4666d6997619a5c3f679c81352f1e8c2c
Gustavo Mendes <g.mendes93@hotmail.com>
2021-08-27 00:39:24 -0300

    feature:Estruturando o Header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown, Image } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
 import LogoBlue from '../assets/images/tex-manager-blue.svg';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Avatar, Row, Col, Space } from 'antd';
+
+import { UserOutlined } from '@ant-design/icons';
 
 import '../components/header.css'
 import SearchTop from './searchTop';
@@ -13,10 +15,26 @@ function HeaderTop() {
     return (
 
     <React.Fragment>
-         <Header className="header fixed">
-             <Image src={LogoBlue} fluid  width="112" height="auto"/>
-             {/* <p>A Fábrica, Confecção Têxtil.</p> */}
-             {/* <SearchTop /> */}
+         <Header className="header fixed">             
+             <Row>
+                <Col flex={1}>
+                    <Image src={LogoBlue} fluid  width="112" height="auto"/>
+                </Col>
+                <Col flex={1}>
+                    <p>A Fábrica, Confecção Têxtil.</p>
+                </Col>
+                             
+                <Col flex={9} className="searchTop">
+                                 <SearchTop />
+                </Col>
+                <Col flex={1} className="icon-Header">
+                   <Space>
+                    <Avatar size="small" icon={<UserOutlined />} />
+                    <Avatar size="small" icon={<UserOutlined />} />
+                    <Avatar size="small" icon={<UserOutlined />} />
+                   </Space>
+                </Col>
+            </Row>
         </Header>
     </React.Fragment>
 
